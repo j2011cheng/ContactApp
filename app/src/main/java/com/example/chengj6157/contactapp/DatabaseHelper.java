18c152e81cd4ae031cf20838fd03ca4676b2c69e
juice-tin <j2011cheng@gmail.com>
2017-05-16 11:09:27 -0700

    Justin Cheng
    added age and address fields

---
+++ app/src/main/java/com/example/chengj6157/contactapp/DatabaseHelper.java
@@ -0,0 +1,53 @@
+package com.example.chengj6157.contactapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.opengl.GLSurfaceView;
+import android.provider.ContactsContract;
+
+/**
+ * Created by chengj6157 on 5/11/2017.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper{
+    public static final String DATABASE_NAME = "Contact.db";
+    public static final String TABLE_NAME = "contact_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "AGE";
+    public static final String COL_4 = "ADDRESS";
+
+    public DatabaseHelper (Context context){
+        super(context, DATABASE_NAME, null, 3);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE TEXT, ADDRESS TEXT) ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+    }
+
+    public boolean insertData(String col, String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col, name);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == 1)return false;
+        else return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+}
--- app/src/main/java/com/example/chengj6157/contactapp/MainActivity.java
+++ app/src/main/java/com/example/chengj6157/contactapp/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.chengj6157.contactapp;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText editName;
+    EditText editAge;
+    EditText editAddress;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+
+
+        editName = (EditText) findViewById(R.id.editText_name);
+        editAge = (EditText) findViewById(R.id.editText_age);
+        editAddress = (EditText) findViewById(R.id.editText_address);
+    }
+
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(myDb.COL_2, editName.getText().toString()) &&
+                myDb.insertData(myDb.COL_3, editAge.getText().toString()) &&
+                myDb.insertData(myDb.COL_4, editAddress.getText().toString());
+
+        if(isInserted == true){
+            Log.d("MyContact", "Successfully inserted " + editName.getText().toString() + " " + editAge.getText().toString() + " " + editAddress.getText().toString());
+        }
+        else{
+            Log.d("MyContact", "Failure inserting data");
+        }
+    }
+
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found in database");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        //loop with res using moveToNext
+        //appent COL to buffer
+        //display message using showMessage
+        showMessage("Data", buffer.toString());
+    }
+
+    private void showMessage(String title, String s) {
+        //AlertDialog.Builder
     }
 }
